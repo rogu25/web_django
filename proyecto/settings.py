56a1416ad8fa0dfe98adf09893c3bc89ae349667
rogu25 <ryavar.montesinos@gmail.com>
2024-07-18 22:30:56 -0500

    sexto commit

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'proyecto.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'adatech.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
